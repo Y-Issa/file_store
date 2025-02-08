07392d188a7414be6f0d6e656a79f0b9779c9ef6
Youssef Issa <youssefissa232@gmail.com>
2025-02-08 15:15:59 +0300

    feat: Auth FE

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">
-        FileStore - A simple storage solution you need.
-      </h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">FileStore - A simple storage solution you need.</h1>
     </div>
   );
 }
